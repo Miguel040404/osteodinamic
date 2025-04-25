e817d65f4e9caec0097e5548bfbecf586273a93b
Miguel040404 <mmaldonadocobo4@gmail.com>
2025-04-25 12:41:42 +0200

    prueba de pull

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -24,14 +24,14 @@ async function page({ searchParams }) {
 
   return (
     <>
-     <div className="mx-auto mt-10 max-w-sm p-8 border border-slate-300 rounded-2xl bg-white shadow-lg">
+     <div className="mx-auto mt-5 max-w-sm p-8 border border-slate-300 rounded-2xl bg-white shadow-lg">
   {error && (
     <div className="mb-4 text-red-600 font-medium">
       {errors.get(error)}
     </div>
   )}
   
-  <h1 className="text-3xl font-bold text-center mb-6">Iniciar sesión</h1>
+  <h1 className="text-3xl font-bold text-center mb-1">Iniciar sesión</h1>
   
   <LoginForm />
   
--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 function page() {
   return (
-    <div className="mx-auto mt-10 max-w-sm p-8 border border-slate-300 rounded-2xl bg-white shadow-lg">
-      <h1 className="text-3xl font-bold text-center mb-6">Crear cuenta</h1>
+    <div className="mx-auto mt-5 max-w-sm p-8 border border-slate-300 rounded-2xl bg-white shadow-lg">
+      <h1 className="text-3xl font-bold text-center mb-1">Crear cuenta</h1>
       <RegisterForm />
       <p className="mt-6 text-center text-sm text-slate-600">
         ¿Ya tienes cuenta? {" "}
