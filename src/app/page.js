c498359c2e6832aef288bac650beef8c32ace4fa
PaniSoto <mpanisoto@gmail.com>
2025-05-13 12:28:05 +0200

    pa miguel

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,3 @@
-
-import { redirect } from "next/navigation"; // IMPORTANTE: importar desde next/navigation
-
-export default function RootPage() {
-  redirect('/auth/login')
-}
-
-
 // import { redirect } from "next/navigation"; // IMPORTANTE: importar desde next/navigation
 
 
