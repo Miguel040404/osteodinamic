ff2bd81c3e73c16022a42bb1021282ad2f3357a0
Miguel040404 <mmaldonadocobo4@gmail.com>
2025-05-27 18:11:37 +0200

    otra vez el login

--- src/components/auth/login-form.js
+++ src/components/auth/login-form.js
@@ -252,7 +252,6 @@ function LoginForm() {
               id="password"
               name="password"
               type="password"
-              minLength="6"
               placeholder="••••••"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               required
