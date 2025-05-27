a48d1bf2547a18075c1948e14acc22ed51d3cc1a
Miguel040404 <mmaldonadocobo4@gmail.com>
2025-05-27 12:04:39 +0200

    prueba de fallo vercel

--- src/app/api/admin/users/[id]/route.js
+++ src/app/api/admin/users/[id]/route.js
@@ -1,5 +1,37 @@
-import { getServerSession } from "next-auth";
-// import { authOptions } from "@/lib/auth";
+// // import { getServerSession } from "next-auth";
+// import { getServerSession } from "next-auth/next";
+// // import { authOptions } from "@/lib/auth";
+// // import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import { NextResponse } from "next/server";
+// import { getUserById } from "@/lib/data";
+
+// export async function GET(req, { params }) {
+//   const session = await getServerSession(authOptions);
+
+//   // Verificar que haya sesión y que el usuario sea admin
+//   if (!session || session.user.role !== "admin") {
+//     return NextResponse.json({ error: "No autorizado" }, { status: 403 });
+//   }
+
+//   const userId = params.id;
+//   const user = await getUserById(userId);
+
+//   if (!user) {
+//     return NextResponse.json({ error: "Usuario no encontrado" }, { status: 404 });
+//   }
+
+//   return NextResponse.json({
+//     id: user.id,
+//     name: user.name,
+//     email: user.email,
+//     phone: user.phone,
+//     address: user.address,
+//     active: user.active,
+//     role: user.role,
+//   });
+// }
+
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
 import { getUserById } from "@/lib/data";
@@ -7,7 +39,6 @@ import { getUserById } from "@/lib/data";
 export async function GET(req, { params }) {
   const session = await getServerSession(authOptions);
 
-  // Verificar que haya sesión y que el usuario sea admin
   if (!session || session.user.role !== "admin") {
     return NextResponse.json({ error: "No autorizado" }, { status: 403 });
   }
