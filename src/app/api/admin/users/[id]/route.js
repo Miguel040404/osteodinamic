d3e2f0f6b9f7e1580b360ecec880cc24900f1e9a
Miguel040404 <mmaldonadocobo4@gmail.com>
2025-05-27 12:00:59 +0200

    fallo de vercel

--- src/app/api/admin/users/[id]/route.js
+++ src/app/api/admin/users/[id]/route.js
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+// import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
 import { getUserById } from "@/lib/data";
 
