cc76d370a8711aafbb784f6eb004109a4a44a8cc
Miguel040404 <mmaldonadocobo4@gmail.com>
2025-06-12 13:16:54 +0200

    Derechos de sesiones

--- src/app/clases/entrenamiento_personal/[entrenamiento_personal]/page.js
+++ src/app/clases/entrenamiento_personal/[entrenamiento_personal]/page.js
@@ -4,6 +4,7 @@ import ListaHorarios from '@/components/horarios/HorarioList';
 import { LoadingSpinner } from '@/components/LoadingSpinner';
 import { auth } from '@/auth';
 import Footer from '@/components/Footer';
+import { hasPaidSession } from '@/lib/data';
 
 
 export default async function ClasePage() {
@@ -13,14 +14,35 @@ export default async function ClasePage() {
     redirect('/auth/login');
   }
 
+  const hasAccess = await hasPaidSession(session.user.id, 'Entrenamiento_personal');
+
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-[#f9faf5]">
       <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 flex items-center justify-center">
-        <Suspense fallback={<LoadingSpinner />}>
-          <ListaHorarios tipo="Entrenamiento_personal" />
-        </Suspense>
+        {hasAccess ? (
+          <Suspense fallback={<LoadingSpinner />}>
+            <ListaHorarios tipo="Entrenamiento_personal" />
+          </Suspense>
+        ) : (
+          <div className="text-center py-12">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Parece que esta sesión no esta habilitada en tu cuentas en este momento.
+            </h2>
+            <p className="text-lg text-gray-600 mb-8">
+              Si necesitas más información o ayuda, no dudes en contactarnos.
+            </p>
+            <a
+              href="/home"
+              className="bg-[#a57551] hover:bg-[#78493c] text-white font-medium py-2 px-4 rounded-lg transition"
+            >
+              Volver a la página principal
+            </a>
+          </div>
+        )}
       </main>
-      <Footer />
+      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <Footer />
+      </div>
     </div>
   );
 }
